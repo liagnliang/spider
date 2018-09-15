11160508a339e054f9704887da319db14a803f1f
liagnliang <34611339@qq.com>
2018-09-15 22:00:59 +0800

    批处理爬取视频
    在bat_spider.py文件中增加爬取的地址列表，可以实现批量爬取功能

---
+++ bat_spider/bat_spider.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+os.system("python E:\PycharmProjects\liagn\spider_icoolxue.py 99")
+time.sleep(10)
+
+os.system("python E:\PycharmProjects\liagn\spider_icoolxue.py 246")
+time.sleep(10)
